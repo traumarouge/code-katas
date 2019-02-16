4bcf55509f772679ef357b0ef7285b61d06f3be7
traumarouge <traumarouge@gmail.com>
2019-02-16 20:26:38 +0100

    berlinclock [Kotlin]: update Kotlin Gradle plugin / update JUnit
    Kotlin Gradle plugin: 1.3.21
    JUnit: 5.4.0

--- berlinclock/kotlin/build.gradle.kts
+++ berlinclock/kotlin/build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.11"
+    kotlin("jvm") version "1.3.21"
 }
 
 version = "0.1.0"
 
-val junitJupiterVersion = "5.3.2"
+val junitJupiterVersion = "5.4.0"
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
@@ -22,7 +22,5 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
 }
